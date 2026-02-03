ca0f10b98c55c27ad0ae62dfd4702c6b6d63cc6e
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2026-02-03 19:34:23 +0100

    Fog II

--- common/src/lib/resources/assets/flywheel/flywheel/fog/linear.glsl
+++ common/src/lib/resources/assets/flywheel/flywheel/fog/linear.glsl
@@ -1,12 +1,23 @@
-vec4 linearFog(vec4 color, float distance, float fogStart, float fogEnd, vec4 fogColor) {
-    if (distance <= fogStart) {
-        return color;
+/* shaders/include/fog.glsl */
+float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
+    if (vertexDistance <= fogStart) {
+        return 0.0;
+    } else if (vertexDistance >= fogEnd) {
+        return 1.0;
     }
 
-    float fogValue = distance < fogEnd ? smoothstep(fogStart, fogEnd, distance) : 1.0;
-    return vec4(mix(color.rgb, fogColor.rgb, fogValue * fogColor.a), color.a);
+    return (vertexDistance - fogStart) / (fogEnd - fogStart);
+}
+
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    return max(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
+}
+
+vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
+    float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmantalEnd, renderDistanceStart, renderDistanceEnd);
+    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
 }
 
 vec4 flw_fogFilter(vec4 color) {
-    return linearFog(color, flw_sphericalDistance, flw_fogRenderDistanceRange.x, flw_fogRenderDistanceRange.y, flw_fogColor);
+    return apply_fog(color, flw_sphericalDistance, flw_cylindricalDistance, flw_fogEnvironmentalRange.x, flw_fogEnvironmentalRange.y, flw_fogRenderDistanceRange.x, flw_fogRenderDistanceRange.y, flw_fogColor);
 }
