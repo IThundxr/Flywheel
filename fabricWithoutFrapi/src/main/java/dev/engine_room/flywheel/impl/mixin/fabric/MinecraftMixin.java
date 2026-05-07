25c6a8c38678536c52f7ba5191da05bc10035b9e
IThundxr <me@ithundxr.dev>
2026-05-07 19:47:33 -0400

    Create fabricWithoutFrapi subproject to test without frapi present

---
+++ fabricWithoutFrapi/src/main/java/dev/engine_room/flywheel/impl/mixin/fabric/MinecraftMixin.java
@@ -0,0 +1,38 @@
+package dev.engine_room.flywheel.impl.mixin.fabric;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import dev.engine_room.flywheel.lib.util.LevelAttached;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+@Mixin(Minecraft.class)
+abstract class MinecraftMixin {
+	@Shadow
+	public ClientLevel level;
+
+	@Inject(method = "setLevel(Lnet/minecraft/client/multiplayer/ClientLevel;)V", at = @At("HEAD"))
+	private void flywheel$onSetLevel(CallbackInfo ci) {
+		if (level != null) {
+			LevelAttached.invalidateLevel(level);
+		}
+	}
+
+	@Inject(method = "disconnect(Lnet/minecraft/client/gui/screens/Screen;ZZ)V", at = @At("HEAD"))
+	private void flywheel$onDisconnect(CallbackInfo ci) {
+		if (level != null) {
+			LevelAttached.invalidateLevel(level);
+		}
+	}
+
+	@Inject(method = "clearClientLevel", at = @At("HEAD"))
+	private void flywheel$onClearClientLevel(CallbackInfo ci) {
+		if (level != null) {
+			LevelAttached.invalidateLevel(level);
+		}
+	}
+}
