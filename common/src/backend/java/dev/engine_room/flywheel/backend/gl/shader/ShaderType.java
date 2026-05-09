c54409a17a4e1860bdbd6fed766a18ce323b4f7d
IThundxr <me@ithundxr.dev>
2026-05-08 21:55:19 -0400

    Deprecate GL classes and delete GlVertexArray & co

--- common/src/backend/java/dev/engine_room/flywheel/backend/gl/shader/ShaderType.java
+++ common/src/backend/java/dev/engine_room/flywheel/backend/gl/shader/ShaderType.java
@@ -4,7 +4,7 @@ import org.lwjgl.opengl.GL43;
 
 import com.mojang.blaze3d.opengl.GlConst;
 
-// TODO: Waiting for mojang to impl compute shaders
+// TODO: Waiting for mojang to impl a enum for compute shaders
 @Deprecated(forRemoval = true)
 public enum ShaderType {
 	VERTEX("vertex", "VERTEX_SHADER", "vert", GlConst.GL_VERTEX_SHADER),
